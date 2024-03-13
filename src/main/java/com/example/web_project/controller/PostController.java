67626e63c35b2fda5ffb3243caeeb388a2393d14
KwonYeonghoo <kupa10000@naver.com>
2024-03-13 18:58:29 +0900

    security 수정(찐막)

--- src/main/java/com/example/web_project/controller/PostController.java
+++ src/main/java/com/example/web_project/controller/PostController.java
@@ -81,27 +81,4 @@ public class PostController {
         return "/bootstrapPost/post";
     }
 
-    @GetMapping("/index")
-    public String boardList(Model model, @PageableDefault(page = 0,size= 5, sort="postDate" ) Pageable pageable) {
-        model.addAttribute("lt", postService.getAllPost(pageable));
-        
-        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
-        model.addAttribute("next", pageable.next().getPageNumber());
-        model.addAttribute("check", postService.getListCheck(pageable));
-
-        
-        return "/bootstrapMain/index";
-    }
-
-    // @GetMapping("/user/index")
-    // public String boardListUser(Model model, @PageableDefault(page = 0,size= 5, sort="postDate" ) Pageable pageable) {
-    //     model.addAttribute("lt", postService.getAllPost(pageable));
-        
-    //     model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
-    //     model.addAttribute("next", pageable.next().getPageNumber());
-    //     model.addAttribute("check", postService.getListCheck(pageable));
-
-        
-    //     return "/bootstrapMain/user/index";
-    // }
 }
--- src/main/java/com/example/web_project/controller/UserController.java
+++ src/main/java/com/example/web_project/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.web_project.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.web_project.model.DTO.PostDto;
 import com.example.web_project.model.DTO.UserDto;
 import com.example.web_project.service.UserService;
 import com.example.web_project.service.impl.PostServiceImpl;
@@ -34,14 +38,37 @@ public class UserController {
     public String userIndexPage(Authentication authentication, Model model, @PageableDefault(page = 0,size= 5, sort="postDate" ) Pageable pageable) {
         
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        log.info("[WebController][boardListUser] userDetails >>" + userDetails.getUsername());
+        log.info("[UserController][userDetails] userName >> " + userDetails.getUsername());
         
         model.addAttribute("lt", postService.getAllPost(pageable));
+
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
         model.addAttribute("check", postService.getListCheck(pageable));
 
         return "/bootstrapMain/user/index";
+    }
+
+    @GetMapping("/user/write")
+    public String userWritePage(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        log.info("[UserController][userWrite] userName >> " + userDetails.getUsername());
+        return "/bootstrapMain/user/write";
+    }
+
+    @PostMapping("/user/write")
+    public String userWrite(@Valid @ModelAttribute PostDto dto, MultipartFile file, Authentication authentication) throws Exception{
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        log.info("[UserController][userWritePage] userDetails >>" + userDetails.getUsername());
+
+        Date now = new Date();
+        dto.setPostDate(now);
+        
+        dto.setPostWriter(String.valueOf(Math.random()));
+        postService.insertPost(dto, file);
+
+        return "redirect:/v1/web/user/index";
     }
 
     // @GetMapping("/user/write")
